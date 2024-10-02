3027244b4da3e1a1b83d75015e0f10828ed29223
ViktorRomanenko05 <1180505n@gmail.com>
2024-10-02 12:49:13 +0200

    homework_first_commit

---
+++ back.js
@@ -0,0 +1,3 @@
+const buttonBack = document.querySelector(".button-back");
+
+buttonBack.addEventListener("click", () => {window.location = "index.html"})
---
+++ index.js
@@ -0,0 +1,13 @@
+const buttonOne = document.querySelector("#button-first");
+const buttonTwo = document.querySelector("#button-second");
+const buttonThree = document.querySelector("#button-third");
+const buttonFour = document.querySelector("#button-fourth");
+const buttonFive = document.querySelector("#button-fifth");
+//const buttonBack = document.querySelector(".button-back");
+
+buttonOne.addEventListener("click", () => {window.location = "yulya_dietz.html"});
+buttonTwo.addEventListener("click", () => {window.location = "lina_goebel.html"});
+buttonThree.addEventListener("click", () => {window.location = "tatiana_n.html"});
+buttonFour.addEventListener("click", () => {window.location = "viktor_romanenko.html"});
+buttonFive.addEventListener("click", () => {window.location = "stanislav_borysenko.html"});
+//buttonBack.addEventListener("click", () => {window.location = "index.html"})
